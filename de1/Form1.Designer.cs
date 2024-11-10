a3cc13cf692915ae911275a11ee912e44dbaa19d
hai12dz <fifahayrataolahai1762004@gmail.com>
2024-11-10 07:59:34 +0700

    de1

---
+++ de1/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace de1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxGhiChu = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAnh = new System.Windows.Forms.Button();
+            this.soLuong = new System.Windows.Forms.TextBox();
+            this.giaBan = new System.Windows.Forms.TextBox();
+            this.giaNhap = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.textBoxGhiChu);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.btnAnh);
+            this.panel1.Controls.Add(this.soLuong);
+            this.panel1.Controls.Add(this.giaBan);
+            this.panel1.Controls.Add(this.giaNhap);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(46, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(716, 297);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(521, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 73);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxGhiChu
+            // 
+            this.textBoxGhiChu.Location = new System.Drawing.Point(490, 213);
+            this.textBoxGhiChu.Multiline = true;
+            this.textBoxGhiChu.Name = "textBoxGhiChu";
+            this.textBoxGhiChu.Size = new System.Drawing.Size(207, 57);
+            this.textBoxGhiChu.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(406, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ghi chú";
+            // 
+            // btnAnh
+            // 
+            this.btnAnh.Location = new System.Drawing.Point(396, 33);
+            this.btnAnh.Name = "btnAnh";
+            this.btnAnh.Size = new System.Drawing.Size(75, 23);
+            this.btnAnh.TabIndex = 12;
+            this.btnAnh.Text = "Ảnh";
+            this.btnAnh.UseVisualStyleBackColor = true;
+            this.btnAnh.Click += new System.EventHandler(this.btnAnh_Click);
+            // 
+            // soLuong
+            // 
+            this.soLuong.Location = new System.Drawing.Point(142, 254);
+            this.soLuong.Name = "soLuong";
+            this.soLuong.Size = new System.Drawing.Size(146, 22);
+            this.soLuong.TabIndex = 11;
+            this.soLuong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.soLuong_KeyPress);
+            // 
+            // giaBan
+            // 
+            this.giaBan.Location = new System.Drawing.Point(142, 213);
+            this.giaBan.Name = "giaBan";
+            this.giaBan.Size = new System.Drawing.Size(146, 22);
+            this.giaBan.TabIndex = 10;
+            this.giaBan.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.giaBan_KeyPress);
+            // 
+            // giaNhap
+            // 
+            this.giaNhap.Location = new System.Drawing.Point(142, 168);
+            this.giaNhap.Name = "giaNhap";
+            this.giaNhap.Size = new System.Drawing.Size(146, 22);
+            this.giaNhap.TabIndex = 9;
+            this.giaNhap.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.giaNhap_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Số lượng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Giá bán";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Giá nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Đơn vị tính";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên vật liệu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã vật liệu";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 400);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(705, 171);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(133, 671);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(276, 671);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(398, 671);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Location = new System.Drawing.Point(557, 671);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(75, 23);
+            this.btnBoQua.TabIndex = 5;
+            this.btnBoQua.Text = "Bỏ qua";
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 769);
+            this.Controls.Add(this.btnBoQua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox soLuong;
+        private System.Windows.Forms.TextBox giaBan;
+        private System.Windows.Forms.TextBox giaNhap;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAnh;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxGhiChu;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnBoQua;
+    }
+}
+
---
+++ de1/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace de1
+{
+    public partial class Form1 : Form
+    {
+        string connectString = @"Data Source=hai\SQLEXPRESS;Initial Catalog=de1;Integrated Security=True;Encrypt=False";
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adt;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(connectString);
+            loadData();
+
+            // Thiết lập trạng thái nút ban đầu
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnThem.Enabled = true;
+            btnBoQua.Enabled = true;
+        }
+
+        private void loadData()
+        {
+            try
+            {
+                dt.Clear();
+                con.Open(); // Mở kết nối thủ công
+                cmd = new SqlCommand("SELECT * FROM VatLieu", con);
+                adt = new SqlDataAdapter(cmd);
+                adt.Fill(dt);
+                con.Close(); // Đóng kết nối thủ công
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+        }
+
+
+        private void btnAnh_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            openFileDialog1.Title = "Chọn ảnh đại diện";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string imagePath = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(imagePath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có muốn thoát không?", "Xác nhận", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void giaNhap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void giaBan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void soLuong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ dữ liệu.");
+                return;
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row["MaVL"].ToString() == textBox1.Text)
+                {
+                    MessageBox.Show("Mã vật liệu đã tồn tại. Vui lòng nhập mã khác.");
+                    return;
+                }
+            }
+
+            try
+            {
+                con.Open(); // Mở kết nối thủ công
+                cmd = new SqlCommand("INSERT INTO VatLieu (MaVL, TenVatLieu, DonViTinh, GiaNhap, GiaBan, SoLuong, Anh, GhiChu) VALUES (@MaVL, @TenVatLieu, @DonViTinh, @GiaNhap, @GiaBan, @SoLuong, @Anh, @GhiChu)", con);
+                cmd.Parameters.AddWithValue("@MaVL", textBox1.Text);
+                cmd.Parameters.AddWithValue("@TenVatLieu", textBox2.Text);
+                cmd.Parameters.AddWithValue("@DonViTinh", textBox3.Text);
+                cmd.Parameters.AddWithValue("@GiaNhap", giaNhap.Text);
+                cmd.Parameters.AddWithValue("@GiaBan", giaBan.Text);
+                cmd.Parameters.AddWithValue("@SoLuong", soLuong.Text);
+                cmd.Parameters.AddWithValue("@Anh", openFileDialog1.FileName);
+                cmd.Parameters.AddWithValue("@GhiChu", textBoxGhiChu.Text);
+
+                cmd.ExecuteNonQuery();
+                con.Close(); // Đóng kết nối thủ công
+
+                MessageBox.Show("Thêm vật liệu thành công!");
+                loadData();
+                clearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi thêm vật liệu: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = row.Cells["MaVL"].Value.ToString();
+                textBox2.Text = row.Cells["TenVatLieu"].Value.ToString();
+                textBox3.Text = row.Cells["DonViTinh"].Value.ToString();
+                giaNhap.Text = row.Cells["GiaNhap"].Value.ToString();
+                giaBan.Text = row.Cells["GiaBan"].Value.ToString();
+                soLuong.Text = row.Cells["SoLuong"].Value.ToString();
+                textBoxGhiChu.Text = row.Cells["GhiChu"].Value?.ToString(); // Hiển thị ghi chú
+
+                if (row.Cells["Anh"].Value != DBNull.Value)
+                {
+                    pictureBox1.Image = Image.FromFile(row.Cells["Anh"].Value.ToString());
+                }
+
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
+                btnThem.Enabled = false;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Hiển thị thông báo xác nhận khi người dùng chọn xóa
+            var result = MessageBox.Show("Bạn có muốn xóa vật liệu này không?", "Xác nhận", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    con.Open(); // Mở kết nối thủ công
+                    cmd = new SqlCommand("DELETE FROM VatLieu WHERE MaVL = @MaVL", con);
+                    cmd.Parameters.AddWithValue("@MaVL", textBox1.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close(); // Đóng kết nối thủ công
+
+                    MessageBox.Show("Xóa vật liệu thành công!");
+
+                    // Cập nhật lại lưới dữ liệu
+                    loadData();
+
+                    // Xóa rỗng các TextBox và ảnh đại diện
+                    clearTextBoxes();
+
+                    // Cập nhật lại trạng thái nút
+                    btnSua.Enabled = false;
+                    btnXoa.Enabled = false;
+                    btnThem.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi xóa vật liệu: " + ex.Message);
+                }
+            }
+            else if (result == DialogResult.No)
+            {
+                // Nếu chọn "No", không xóa và thực hiện các bước sau
+
+                // Xóa rỗng các TextBox và ảnh đại diện
+                clearTextBoxes();
+
+                // Cập nhật lại trạng thái nút
+                btnSua.Enabled = false;
+                btnXoa.Enabled = false;
+                btnThem.Enabled = true;
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lấy ảnh cũ từ cơ sở dữ liệu trước khi cập nhật
+                string currentImagePath = GetCurrentImagePath(textBox1.Text);
+
+                // Kiểm tra xem người dùng có chọn ảnh mới không
+                string imagePath = openFileDialog1.FileName;
+
+                // Nếu người dùng không chọn ảnh mới, giữ nguyên ảnh cũ
+                if (string.IsNullOrEmpty(imagePath) || imagePath == "openFileDialog1")
+                {
+                    imagePath = currentImagePath;
+                }
+
+                con.Open(); // Mở kết nối thủ công
+
+                // Cập nhật thông tin vật liệu vào cơ sở dữ liệu
+                cmd = new SqlCommand("UPDATE VatLieu SET TenVatLieu = @TenVatLieu, DonViTinh = @DonViTinh, GiaNhap = @GiaNhap, GiaBan = @GiaBan, SoLuong = @SoLuong, Anh = @Anh, GhiChu = @GhiChu WHERE MaVL = @MaVL", con);
+
+
+                cmd.Parameters.AddWithValue("@MaVL", textBox1.Text);
+                cmd.Parameters.AddWithValue("@TenVatLieu", textBox2.Text);
+                cmd.Parameters.AddWithValue("@DonViTinh", textBox3.Text);
+                cmd.Parameters.AddWithValue("@GiaNhap", giaNhap.Text);
+                cmd.Parameters.AddWithValue("@GiaBan", giaBan.Text);
+                cmd.Parameters.AddWithValue("@SoLuong", soLuong.Text);
+                cmd.Parameters.AddWithValue("@GhiChu", textBoxGhiChu.Text);
+
+                // Cập nhật ảnh: nếu có ảnh mới, dùng ảnh mới; nếu không có, giữ ảnh cũ
+                if (string.IsNullOrEmpty(imagePath))
+                {
+                    cmd.Parameters.AddWithValue("@Anh", DBNull.Value);  // Nếu không có ảnh cũ hoặc mới, đặt giá trị ảnh là NULL
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@Anh", imagePath);  // Cập nhật ảnh mới hoặc giữ ảnh cũ
+                }
+
+                cmd.ExecuteNonQuery();
+                con.Close(); // Đóng kết nối thủ công
+
+                MessageBox.Show("Cập nhật vật liệu thành công!");
+                loadData();
+                clearTextBoxes();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi cập nhật vật liệu: " + ex.Message);
+            }
+        }
+
+        private string GetCurrentImagePath(string maVL)
+        {
+            string currentImagePath = null;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT Anh FROM VatLieu WHERE MaVL = @MaVL", con))
+                    {
+                        cmd.Parameters.AddWithValue("@MaVL", maVL);
+                        object result = cmd.ExecuteScalar();
+                        if (result != DBNull.Value && result != null)
+                        {
+                            currentImagePath = result.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi lấy ảnh cũ: " + ex.Message);
+            }
+
+            return currentImagePath;
+        }
+
+
+        private void clearTextBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBoxGhiChu.Clear();
+            giaNhap.Clear();
+            giaBan.Clear();
+            soLuong.Clear();
+            pictureBox1.Image = null;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnThem.Enabled = true;
+        }
+    }
+}
